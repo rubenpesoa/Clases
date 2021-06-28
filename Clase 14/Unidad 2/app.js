fca27c3fca7e1f0596aa51b0f55e8ecdc96ac1db
ruben pesoa <rubenpesoa@gmail.com>
2021-06-28 10:50:02 -0300

    subo clase 14

---
+++ Clase 14/Unidad 2/app.js
@@ -0,0 +1,54 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var nosotrosRouter = require('./routes/nosotros');
+var serviciosRouter = require('./routes/servicios');
+var contactoRouter = require('./routes/contacto');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/nosotros', nosotrosRouter);
+app.use('/servicios', serviciosRouter);
+app.use('/contacto', contactoRouter);
+
+
+app.get('/prueba', function(req,res){
+  res.send('pagina de prueba traves del get');
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
+
+require('dotenv').config();
---
+++ Clase 14/Unidad 2/routes/contacto.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  res.send('hola soy la pagina de contacto');
+});
+
+module.exports = router;
+
+var nodemailer = require("nodemailer");
+
+router.post('/', async (req, res, next) => {
+
+  var nombre = req.body.nombre;
+  var email = req.body.email;
+  var tel = req.body.tel;
+  var mensaje = req.body.mensaje;
+
+  var obj = {
+    to: 'rubenpesoa@gmail.com',
+    subject: 'CONTACTO WEB',
+    html: nombre + " se contacto a traves de la web y quiere mas infomracion a este correo :" + email + ". <br> ademas, hizo este comentario: " + mensaje + ". <br> Su tel: " + tel
+  }
+
+  var transport = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS
+    }
+  })
+
+  var info = await transport.sendMail(obj);
+
+  res.render('contacto', {
+    message: 'Mensaje enviado correctamente'
+  });
+});
